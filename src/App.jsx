ae7fe29fd4447945a7d5c52beb7c2e346bb53d31
Mark Anthony <touya1313@hotmail.com>
2025-04-03 20:33:17 -0600

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,9 @@ import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Hero from './components/Hero'; 
 
+function App() {
+    
+}
+
 
 export default App;
