3f07fb1e6df8eb633a13934e22c9490412f7b019
Mark Anthony <touya1313@hotmail.com>
2025-04-05 17:06:18 -0600

    hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,11 @@
 export default function Hero({ onBook }) {
-    
+    return (
+        <div className="">
+            <div className="">
+                <div className="" style={{
+                    backgroundImage: 'url("https://images.unsplash.com/photo-1576013551627-0cc20b96c2a7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80")'
+                }}
+            </div>
+        </div>
+    )
 }
