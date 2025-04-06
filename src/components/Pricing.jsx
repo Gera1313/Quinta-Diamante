e94c5ff7507d3ac0ebd1f2028b01bc564e090d2c
Mark Anthony <touya1313@hotmail.com>
2025-04-05 21:54:22 -0600

    added temp code to connect the pages to app.jsx and have it display something

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,8 @@
+export default function Pricing() {
+    return (
+      <section className="py-20 text-white text-center">
+        <h2 className="text-3xl font-bold mb-4">Pricing</h2>
+        <p>Coming soon...</p>
+      </section>
+    );
+  }  
