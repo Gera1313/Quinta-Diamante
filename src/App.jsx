7c1f05b57b299e228eb666393b5d2c89d06ee30b
Mark Anthony <touya1313@hotmail.com>
2025-04-05 20:35:26 -0600

    finished app.jsx with my new component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Hero from './components/Hero'; 
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Amenities from './components/Amenities';
+import Gallery from './components/Gallery';
+import Pricing from './components/Pricing';
+import FAQ from './components/FAQ';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import BookingModal from './components/BookingModal';
 
 function App() {
-    
+  return (
+    <Router>
+      <div className="min-h-screen bg-quinta-black">
+        <Navbar />
+        <main>
+          <Hero />
+          <Amenities />
+          <Gallery />
+          <Pricing />
+          <FAQ />
+          <Contact />
+        </main>
+        <Footer />
+        <BookingModal />
+      </div>
+    </Router>
+  );
 }
-
 
 export default App;
