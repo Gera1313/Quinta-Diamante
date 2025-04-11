8f3614b8aa1bfc835fd62c957ca0505b6b11699e
Mark Anthony <touya1313@hotmail.com>
2025-04-10 21:45:00 -0600

    booking modal

--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -4,7 +4,8 @@ import { XMarkIcon } from '@heroicons/react/24/solid';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
-export default function BookingModal() {
+export default function BookingModal({ isOpen, setIsOpen, initialEventType }) {
+  const 
     return (
       <section className="py-20 text-white text-center">
         <h2 className="text-3xl font-bold mb-4">Booking Modal</h2>
