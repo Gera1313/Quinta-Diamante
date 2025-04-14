380286b31eb57eed619b2034e5b758d6b5125565
Mark Anthony <touya1313@hotmail.com>
2025-04-14 12:58:57 -0600

    transition root

--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -36,9 +36,8 @@ export default function BookingModal({ isOpen, setIsOpen, initialEventType }) {
   };
 
     return (
-      <section className="py-20 text-white text-center">
-        <h2 className="text-3xl font-bold mb-4">Booking Modal</h2>
-        <p>Coming soon...</p>
-      </section>
+      <Transition.Root show={isOpen} as={Fragment}>
+        
+      </Transition.Root>
     );
   }
