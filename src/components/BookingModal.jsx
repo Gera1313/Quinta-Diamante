234a49563d228ee2a965867856eba574b91a10cf
Mark Anthony <touya1313@hotmail.com>
2025-04-14 13:49:58 -0600

    transtion child

--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -37,7 +37,19 @@ export default function BookingModal({ isOpen, setIsOpen, initialEventType }) {
 
     return (
       <Transition.Root show={isOpen} as={Fragment}>
-        
+        <Dialog as="div" className="relative z-50" onClose={setIsOpen}>
+          <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+        </Dialog>
       </Transition.Root>
     );
   }
